63aff201976ba7ff85a49b389290c9cbd685ba9b
mateuzito <mateuszitosilva@gmail.com>
2024-10-21 20:56:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
+downloadBtn = document.getElementById("download-btn");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value.trim();
+  if (!qrValue || preValue === qrValue) return;
+  preValue = qrValue;
+  generateBtn.innerText = "Gerando QR Code...";
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Gerar QR Code";
+
+    downloadBtn.style.display = "inline-block";
+  });
+});
+
+downloadBtn.addEventListener("click", () => {
+  fetch(qrImg.src)
+    .then((res) => res.blob())
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "qrcode.png";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    })
+    .catch((err) => console.error("Erro ao baixar o Qr Code", err));
+});
+
+qrInput.addEventListener("keyup", () => {
+  if (!qrInput.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+    downloadBtn.style.display = "none";
+  }
+});
